41ce37c80fc8709bc022eef5963701ca0a7a825d
Margot Kriete <margot.kriete@gmail.com>
2024-07-08 09:53:36 -0400

    Add support for custom ports

--- browser.py
+++ browser.py
@@ -1,22 +1,37 @@
 import socket
+import ssl
 
 
 class URL:
     def __init__(self, url: str) -> None:
         # parse URL into host, path
         self.scheme, url = url.split("://", 1)
-        assert self.scheme == "http"
+        assert self.scheme in ["http", "https"]
+        if self.scheme == "http":
+            self.port = 80
+        else:
+            self.port = 443
 
         if "/" not in url:
             url = url + "/"
+
         self.host, url = url.split("/", 1)
         self.path = "/" + url
+
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
 
     def request(self) -> str:
         s = (
             socket.socket()
         )  # defaults: addr family AF_INET, type SOCKET_STREAM, protocol IPPROTO_TCP
-        s.connect((self.host, 80))
+        s.connect((self.host, self.port))
+
+        if self.scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.host)
+
         req = f"GET {self.path} HTTP/1.0\r\n"
         req += f"Host: {self.host}\r\n"
         req += "\r\n"
