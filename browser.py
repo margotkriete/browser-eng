877042f8a80ed2ae644e1dfcadfd5203c8c7a771
Margot Kriete <margot.kriete@gmail.com>
2024-07-15 11:37:20 -0400

    Fix scrollbar: draw rectangle instead of using library

--- browser.py
+++ browser.py
@@ -9,58 +9,66 @@ WIDTH, HEIGHT = 800, 600
 SCROLL_STEP = 100
 TEST_FILE = "/Users/margotkriete/Desktop/test.txt"
 HSTEP, VSTEP = 13, 18
+SCROLLBAR_WIDTH = 20
 
 
 class Browser:
     def __init__(self, rtl: bool = False):
         self.window = tkinter.Tk()
-        self._init_scrollbar()
         self.canvas = tkinter.Canvas(
             self.window,
             width=WIDTH,
             height=HEIGHT,
-            yscrollcommand=self.scrollbar.set,
-            scrollregion=(0, 0, HEIGHT, WIDTH),
         )
-        self.scrollbar.config(command=self.scrollbar_handler)
         self.canvas.pack(fill="both", expand=1)
         self.scroll = 0
-
         self.window.bind("<Down>", self.scrolldown)
         self.window.bind("<Up>", self.scrollup)
         self.window.bind("<MouseWheel>", self.mousescroll)
         self.window.bind("<Configure>", self.resize)
         self.screen_height = HEIGHT
+        self.screen_width = WIDTH
         self.rtl = rtl
 
         # Size unit is "points," which are 72nds of an inch, not pixels
         self.bi_times = tkinter.font.Font(family="Times", size=16)
 
-    def _init_scrollbar(self):
-        style = ttk.Style()
-        style.configure("TScrollbar", background="#002fa7")
-        self.scrollbar = ttk.Scrollbar(self.window, style="TScrollbar")
-        self.scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-
-    def _get_max_coordinate(self):
+    def _get_page_height(self):
         return self.display_list[len(self.display_list) - 1][1]
-
-    # Exercise 2.4: WIP, does not work properly
-    def scrollbar_handler(self, *opts):
-        op, quantity = opts[0], opts[1]
-        updated_scroll = self.scroll + float(quantity) * HEIGHT
-        if updated_scroll < self._get_max_coordinate():
-            self.scroll = updated_scroll
-            self.draw()
 
     def load(self, url: str) -> None:
         body = url.request()
         self.text = lex(body)
         self.display_list = layout(self.text, rtl=self.rtl)
+        self.draw_scrollbar()
         self.draw()
 
-    def draw(self, font=None):
-        self.canvas.delete("all")
+    # Exercise 2.4
+    def get_scrollbar_coordinates(self):
+        page_height = self._get_page_height()
+        scrollbar_height = (self.screen_height / page_height) * self.screen_height
+        if self.display_list:
+            x0 = self.screen_width - SCROLLBAR_WIDTH
+            y0 = (self.scroll / page_height) * self.screen_height
+            x1 = self.screen_width
+            y1 = y0 + scrollbar_height
+            return x0, y0, x1, y1
+
+    # Exercise 2.4
+    def draw_scrollbar(self) -> None:
+        if self.display_list:
+            if self._get_page_height() < self.screen_height:
+                return
+            x0, y0, x1, y1 = self.get_scrollbar_coordinates()
+            if self.canvas.gettags("scrollbar"):
+                self.canvas.coords("scrollbar", x0, y0, x1, y1)
+            else:
+                self.canvas.create_rectangle(
+                    x0, y0, x1, y1, fill="blue", tags="scrollbar"
+                )
+
+    def draw(self, font=None) -> None:
+        self.canvas.delete("text")
         for x, y, c, font in self.display_list:
             if not font:
                 font = self.bi_times
@@ -68,40 +76,46 @@ class Browser:
                 continue
             if y + VSTEP < self.scroll:
                 continue
-            self.canvas.create_text(x, y - self.scroll, text=c, font=font, anchor="nw")
+            self.canvas.create_text(
+                x, y - self.scroll, text=c, font=font, anchor="nw", tag="text"
+            )
 
     def scrolldown(self, e):
-        updated_scroll = self.scroll + SCROLL_STEP + self.screen_height
-        if updated_scroll < self._get_max_coordinate():
+        if not self.display_list:
+            return
+        if self.scroll + self.screen_height < self._get_page_height():
             self.scroll += SCROLL_STEP
             self.draw()
+            self.draw_scrollbar()
 
     def scrollup(self, e):
         if self.scroll >= SCROLL_STEP:
             self.scroll -= SCROLL_STEP
         self.draw()
+        self.draw_scrollbar()
 
     # Exercise 2.2
     def mousescroll(self, e):
         updated_scroll = self.scroll - e.delta + self.screen_height
-        if (updated_scroll < self._get_max_coordinate()) and (
-            self.scroll - e.delta > 0
-        ):
+        if (updated_scroll < self._get_page_height()) and (self.scroll - e.delta > 0):
             self.scroll -= e.delta
             self.draw()
+            self.draw_scrollbar()
 
     # Exercise 2.3
     def resize(self, e):
         self.screen_height = e.height
-        self.display_list = layout(self.text, e.width, self.rtl)
+        self.screen_width = e.width
+        self.display_list = layout(self.text, self.screen_width, self.rtl)
         self.draw()
+        self.draw_scrollbar()
 
 
 def layout(
     tokens: "list[str]", width: int = WIDTH, rtl: bool = False
 ) -> "list[tuple[int, int, str]]":
-    # if rtl:
-    # return layout_rtl(text, width)
+    if rtl:
+        return layout_rtl(tokens, width)
 
     cursor_x, cursor_y = HSTEP, VSTEP
     weight, style = "normal", "roman"
@@ -116,7 +130,7 @@ def layout(
                 cursor_x += w + font.measure(" ")
 
                 # Wrap text once we reach the edge of the screen
-                if cursor_x + w >= width - HSTEP:
+                if cursor_x + w >= width - HSTEP - SCROLLBAR_WIDTH:
                     cursor_y += font.metrics("linespace") * 1.25
                     cursor_x = HSTEP
 
@@ -137,10 +151,12 @@ def layout(
 
 
 # Exercise 2.7
+# TODO: fix after working through Chapter 3
 def layout_rtl(text: str, width: int = WIDTH):
     cursor_x = width - HSTEP
     cursor_y = VSTEP
     display_list = []
+    font = tkinter.font.Font(family="Times", size=16)
 
     # Keep track of current line in list, and when you reach
     # a new line, append existing list to display_list, but lay out in reverse
@@ -157,7 +173,7 @@ def layout_rtl(text: str, width: int = WIDTH):
             cursor_x = width - HSTEP
             line.reverse()
             for l in line:
-                display_list.append((cursor_x, cursor_y, l))
+                display_list.append((cursor_x, cursor_y, l, font))
                 cursor_x -= HSTEP
             cursor_y += VSTEP
 
